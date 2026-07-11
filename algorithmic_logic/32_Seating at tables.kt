2476f8d88bddd3d8b73161b1b44ffa75ad38a7f4
Kolomeitseva Alexandra <aleksa.nik199@gmail.com>
2026-07-11 14:55:45 +0200

    Update 32_Seating at tables.kt

--- algorithmic_logic/32_Seating at tables.kt
+++ algorithmic_logic/32_Seating at tables.kt
@@ -6,67 +6,36 @@ fun main() {
         val (n, x, s) = readln().trim().split(" ").map { it.toInt() }
         val u = readln()
 
-        val seatedCount = seatingOfGuests(n, x, s, u)
-
-        println(seatedCount)
+        val countA = u.count() {it == 'A'}
+        var best = 0
+        for (k in 0..countA) {
+            val result = seatingOfGuests(n, x, s, u, k)
+            best = maxOf(best, result)
+        }
+        println(best)
     }
 }
 
-fun seatingOfGuests(n: Int, x: Int, s: Int, u: String): Int {
-    val tables = MutableList(x) { 0 }
-    var count = 0
-    var remainingIntroverts = u.count { it == 'I' }
+fun seatingOfGuests(n: Int, x: Int, s: Int, u: String, k: Int): Int {
+    var ambiverts = 0
+    var opened = 0
+    var seated = 0
 
     for (person in u) {
         when (person) {
-            'I' -> {
-                val idx = tables.indexOfFirst { it == 0 }
-                if (idx != -1) {
-                    tables[idx] = 1
-                    count++
-                }
-                remainingIntroverts--
-            }
-
-            'E' -> {
-                val idx = tables.indexOfFirst { it in 1 until s }
-                if (idx != -1) {
-                    tables[idx]++
-                    count++
-                }
-            }
-
+            'I' -> if (opened < x) {opened++; seated++}
+            'E' -> if (seated < opened * s) {seated++}
             'A' -> {
-                if (remainingIntroverts > 0) {
-                    val idxNonEmpty = tables.indexOfFirst { it in 1 until s }
-                    if (idxNonEmpty != -1) {
-                        tables[idxNonEmpty]++
-                        count++
-                    } else {
-                        val idxEmpty = tables.indexOfFirst { it == 0 }
-                        if (idxEmpty != -1) {
-                            tables[idxEmpty] = 1
-                            count++
-                        }
-                    }
+                ambiverts++
+                if (ambiverts <= k) {
+                    if (opened < x) {opened++; seated++}
                 } else {
-                    val idxEmpty = tables.indexOfFirst { it == 0 }
-                    if (idxEmpty != -1) {
-                        tables[idxEmpty]++
-                        count++
-                    } else {
-                        val idxEmpty = tables.indexOfFirst { it in 1 until s }
-                        if (idxEmpty != -1) {
-                            tables[idxEmpty]++
-                            count++
-                        }
-                    }
+                    if (seated < opened * s) {seated++}
                 }
             }
         }
     }
-    return count
+    return seated
 }
-
 
 
